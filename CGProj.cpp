1dc31fa65869c6b6437f512264cdc4407ed917dc
dario-loi <77234595+dario-loi@users.noreply.github.com>
2022-12-12 17:14:52 +0100

    Initial commit

---
+++ CGProj.cpp
@@ -0,0 +1,24 @@
+﻿/*****************************************************************//**
+ * \file   CGProj.cpp
+ * \brief  Main application for Computer graphics project.
+ * 
+ * \author Dario Loi
+ * \date   December 2022
+ *********************************************************************/
+
+#include "vendor/CImg.h"
+
+const constexpr size_t X = 256;
+const constexpr size_t Y = 256;
+
+int main()
+{
+	namespace imglib = cimg_library;
+
+	imglib::CImg<float> img{ 256, 256, 1, 3 };
+
+	img.draw_text(X / 2, Y / 2, "Hello World!", "white", 1.0f, 14);
+	img.display();
+
+	return 0;
+}
---
+++ hdr.h
@@ -0,0 +1,86 @@
+/*****************************************************************//**
+ * \file   hdr.h
+ * \brief  functions for HDR
+ * 
+ * \author Dario Loi
+ * \date   December 2022
+ *********************************************************************/
+
+#pragma once
+
+#include "vendor/CImg.h"
+using namespace cimg_library;
+
+/**
+ * @brief Map LDR images to HDR
+ * 
+ * @tparam T 
+ * @param LDR_images 
+ * @return CImg<T> 
+ */
+template<typename T>
+CImg<T> LDR_to_HDR(CImg<T> const& LDR_images);
+
+/**
+ * @brief Compute the weights for each LDR image
+ * 
+ * @tparam T the integral type of the pixel
+ * @param LDR_images a CImg<T> object containing the LDR images
+ * @return CImg<T> a CImg<T> object containing the weights for each LDR image
+ */
+template<typename T> 
+CImg<T> compute_W(CImg<T> const& LDR_images);
+
+/**
+ * @brief Compute the Laplacian pyramid of the LDR images
+ * 
+ * @tparam T the integral type of the pixel
+ * @param LDR_images a CImg<T> object containing the LDR images
+ * @return CImg<T> a CImg<T> object containing the Laplacian pyramid of the LDR images
+ */
+template<typename T>
+CImg<T> compute_contrast(CImg<T> const& LDR_images);
+
+/**
+ * @brief Compute the saturation of the LDR images
+ * 
+ * @tparam T the integral type of the pixel
+ * @param LDR_images a CImg<T> object containing the LDR images
+ * @return CImg<T> a CImg<T> object containing the saturation of the LDR images
+ */
+template<typename T>
+CImg<T> compute_saturation(CImg<T> const& LDR_images);
+
+/**
+ * @brief Compute the exposure of the LDR images
+ * 
+ * @tparam T the integral type of the pixel
+ * @param LDR_images 
+ * @return CImg<T> a CImg<T> object containing the exposure of the LDR images
+ */
+ */
+template<typename T>
+CImg<T> compute_wexp(CImg<T> const& LDR_images);
+
+
+/**
+ * @brief Fuse the LDR images
+ * 
+ * @tparam T the integral type of the pixel
+ * @param LDR_images 
+ * @param weights 
+ * @return CImg<T> 
+ */
+template<typename T>
+CImg<T> fuse_LDR(CImg<T> const& LDR_images, CImg<T> const& weights);
+
+/**
+ * @brief Reconstruction of the HDR image from the Laplacian pyramid
+ * 
+ * @tparam T the integral type of the pixel
+ * @param fused_laplacian 
+ * @return CImg<T> 
+ */
+template<typename T>
+CImg<T> reconstruct_pyramid(CImg<T> const& fused_laplacian);
+
