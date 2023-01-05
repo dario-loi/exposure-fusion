e36c71707c7de9e45ed4d8a08bc2fc19db960e63
dario-loi <loi.1940849@studenti.uniroma1.it>
2023-01-05 15:13:49 +0100

    Actual output on the GUI slider

--- gui/image_slider.py
+++ gui/image_slider.py
@@ -3,45 +3,44 @@ from PIL import Image, ImageTk
 
 
 class Application(tk.Frame):
-    
-    
+
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.grid()
         self.create_slider()
 
-
-
     def create_slider(self):
         screenHeight = 600
         screemWidth = 500
         # CREATING IMAGES
-        
-        size = (500, 600)
-        self.img1 = ImageTk.PhotoImage(Image.open("gui/elements/ldr_images.jpg").resize(size))
-        self.canv1 = tk.Canvas(self, width=500, height=600, 
-            highlightthickness=0, bd=0)
+
+        FILL_FACTOR = 1.50
+        size = (int(730 * FILL_FACTOR), int(410 * FILL_FACTOR))
+        self.img1 = ImageTk.PhotoImage(Image.open(
+            "gui/elements/ldr_image.png").resize(size))
+        self.canv1 = tk.Canvas(self, width=500, height=600,
+                               highlightthickness=0, bd=0)
         self.canv1.grid(row=0, column=0, sticky="nsew")
         self.canv1.create_image(500/2, 600/2, image=self.img1, anchor="center")
 
-        self.img2 = ImageTk.PhotoImage(Image.open("gui/elements/hdr_images.jpg").resize(size))
-        self.canv2 = tk.Canvas(self, width=500, height=600, 
-            highlightthickness=0, bd=0)
+        self.img2 = ImageTk.PhotoImage(Image.open(
+            "gui/elements/hdr_image.png").resize(size))
+        self.canv2 = tk.Canvas(self, width=500, height=600,
+                               highlightthickness=0, bd=0)
         self.canv2.grid(row=0, column=0, sticky="nsw")
         self.canv2.create_image(500/2, 600/2, image=self.img2, anchor="center")
 
-        self.line = self.canv2.create_line(500/2, 0, 500/2, 600, 
-            width=4, fill="white")    
+        self.line = self.canv2.create_line(500/2, 0, 500/2, 600,
+                                           width=4, fill="white")
 
         self.canv2.bind('<B1-Motion>', self.slide_image)
-    
 
     def slide_image(self, event):
         cur_length = int(event.x)
         cur_height = int(event.y)
         if cur_height > ((600/2) - 100) and cur_height < ((600/2) + 100):
-            if cur_length < 500: 
+            if cur_length < 500:
                 self.canv2.config(width=cur_length)
                 self.canv2.coords(self.line, cur_length, 0, cur_length, 600)
             else:
@@ -50,12 +49,10 @@ class Application(tk.Frame):
 
 
 def build_slider():
-    #Main function to build the slider
+    # Main function to build the slider
     root = tk.Tk()
     root.state('zoomed')
     root.geometry("%dx%d+0+0" % (500, 600))
     root.title("Image Slider")
     app = Application(master=root)
     app.mainloop()
-    
-
